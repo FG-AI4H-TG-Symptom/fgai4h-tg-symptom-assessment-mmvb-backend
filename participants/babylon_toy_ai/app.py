d6737516ffe14b63b0fcfbc76c2b43502df9e41d
Yura Perov <yura.perov@b-1432.local>
2020-02-18 10:09:38 +0000

    CORS for AIs

--- participants/babylon_toy_ai/app.py
+++ participants/babylon_toy_ai/app.py
@@ -6,6 +6,7 @@
 import sys
 
 import connexion
+from flask_cors import CORS
 
 sys.path.append("../..")  # isort:skip
 from config import CONFIG_DEFAULT_HOST  # isort:skip  # NOQA: E402
@@ -15,6 +16,8 @@ def create_app():
     connexion_app = connexion.App(
         __name__, specification_dir="../../swagger/", debug=True
     )
+    # TODO: revisit CORS
+    CORS(connexion_app.app)
     connexion_app.add_api("toy-ai.yaml", strict_validation=True)
     app = connexion_app.app
 
--- toy_ai/app.py
+++ toy_ai/app.py
@@ -6,6 +6,7 @@
 import sys
 
 import connexion
+from flask_cors import CORS
 
 sys.path.append("..")  # isort:skip
 from config import CONFIG_DEFAULT_HOST  # isort:skip  # NOQA: E402
@@ -13,6 +14,8 @@ from config import CONFIG_DEFAULT_HOST  # isort:skip  # NOQA: E402
 
 def create_app():
     connexion_app = connexion.App(__name__, specification_dir="../swagger/", debug=True)
+    # TODO: revisit CORS
+    CORS(connexion_app.app)
     connexion_app.add_api("toy-ai.yaml", strict_validation=True)
     app = connexion_app.app
 
