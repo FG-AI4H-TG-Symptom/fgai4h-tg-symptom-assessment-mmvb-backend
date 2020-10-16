8d5d0d7b989b65a99adb311402bf5258967ecd0e
lebovka <ivan.lebovka.github@gmail.com>
2020-10-16 15:56:37 +0200

    individual ai responses removed from the endpoints

--- mmvb_backend/benchmarking_sessions/api/utils.py
+++ mmvb_backend/benchmarking_sessions/api/utils.py
@@ -6,3 +6,31 @@ def strip_values_from_responses(responses):
         ].items():
             if "value" in response:
                 del response["value"]
+
+
+def get_stats_table(responses, ai_ids):
+    result = {}
+
+    # create empties for collecting results
+    for ai_id in ai_ids:
+        result[ai_id] = {"errors": 0, "timeouts": 0, "completed": 0}
+
+    # collect results
+    for response in responses:
+        responses_for_case = response["responses"]
+
+        for ai_id in ai_ids:
+            if responses_for_case[ai_id]["status"] == "COMPLETED":
+                result[ai_id]["completed"] = result[ai_id]["completed"] + 1
+            elif (
+                responses_for_case[ai_id]["status"] == "ERRORED"
+                and responses_for_case[ai_id]["error"] == "TIMEOUT"
+            ):
+                result[ai_id]["timeouts"] = result[ai_id]["timeouts"] + 1
+            elif (
+                responses_for_case[ai_id]["status"] == "ERRORED"
+                and responses_for_case[ai_id]["error"] != "TIMEOUT"
+            ):
+                result[ai_id]["errors"] = result[ai_id]["errors"] + 1
+
+    return result
--- mmvb_backend/benchmarking_sessions/api/views.py
+++ mmvb_backend/benchmarking_sessions/api/views.py
@@ -9,7 +9,10 @@ from benchmarking_sessions.api.serializers import (
     BenchmarkingSessionResultsSerializer,
     BenchmarkingSessionSerializer,
 )
-from benchmarking_sessions.api.utils import strip_values_from_responses
+from benchmarking_sessions.api.utils import (
+    get_stats_table,
+    strip_values_from_responses,
+)
 from benchmarking_sessions.models import BenchmarkingSession
 from benchmarking_sessions.tasks import run_benchmark
 from common.utils import CamelCaseAutoSchema
@@ -49,9 +52,11 @@ class BenchmarkingSessionViewSet(ModelViewSet):
     @action(methods=["get"], detail=True, url_path="status")
     def benchmark_status(self, request, *args, **kwargs):
         """Handler for checking benchmark session status"""
+
         benchmarking_session = get_object_or_404(
             BenchmarkingSession, id=kwargs["pk"]
         )
+
         if benchmarking_session.status != BenchmarkingSession.Status.RUNNING:
             return Response(
                 {"status": benchmarking_session.status},
@@ -59,6 +64,7 @@ class BenchmarkingSessionViewSet(ModelViewSet):
             )
 
         result = run_benchmark.AsyncResult(benchmarking_session.task_id)
+
         if result.status == PENDING or result.info is None:
             # this is an in-between state where the celery task status is not yet reflected in the main DB,
             # so either the task is about to begin or just finished -- in either case it's easiest to pretend it is
@@ -70,10 +76,15 @@ class BenchmarkingSessionViewSet(ModelViewSet):
 
         strip_values_from_responses(result.info["responses"])
 
+        ais = benchmarking_session.ai_implementations.all()
+        ai_ids = [str(ai.id) for ai in ais]
+        statistics = result.info["statistics"]
+        statistics["table"] = get_stats_table(result.info["responses"], ai_ids)
+
         return Response(
             {
-                "status": BenchmarkingSession.Status.RUNNING,
-                "report": result.info,
+                "status": benchmarking_session.status,
+                "statistics": statistics,  # {'currentCaseIndex': 30, 'totalCaseCount': 50}
             },
             status=status.HTTP_200_OK,
         )
@@ -94,6 +105,15 @@ class BenchmarkingSessionViewSet(ModelViewSet):
         benchmarking_session_result["metrics"] = calculate_metrics(
             benchmarking_session_result
         )
+
+        ais = benchmarking_session.ai_implementations.all()
+        ai_ids = [str(ai.id) for ai in ais]
+
+        stats_table = get_stats_table(
+            benchmarking_session_result["responses"], ai_ids
+        )
+        benchmarking_session_result["statsTable"] = stats_table
+        del benchmarking_session_result["responses"]
 
         return Response(
             benchmarking_session_result, status=status.HTTP_200_OK,
